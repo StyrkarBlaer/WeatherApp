c8bd6b3b6bfff4ae854e012011b3406f869da987
Styrkár Blær Styrmisson <stykkib@gmail.com>
2023-07-25 04:34:07 +0000

    Some minor changes while i wait for an api key

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import { Temporal } from '@js-temporal/polyfill';
+
 console.log("Hi")
+
+async function getWeatherAndTimezone() {
+  let response = await fetch('api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ab1831ff378ed52138a811ff0f60dc1f')
+  let weather = await response.json()
+  return weather
+}
+
+getWeatherAndTimezone().then(data => console.log(data));
+
+console.log(Temporal.Now.timeZoneId())
