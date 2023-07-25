a518b201055d2a6a81c9b954f838880329d9a59b
Styrkár Blær Styrmisson <stykkib@gmail.com>
2023-07-25 05:29:18 +0000

    playing around with Temporal (wtf)

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,25 @@ import { Temporal } from '@js-temporal/polyfill';
 
 console.log("Hi")
 
-async function getWeatherAndTimezone() {
-  let response = await fetch('api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ab1831ff378ed52138a811ff0f60dc1f')
-  let weather = await response.json()
-  return weather
-}
+//async function getWeatherAndTimezone() {
+ // let response = await fetch('api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ab1831ff378ed52138a811ff0f60dc1f')
+  //let weather = await response.json()
+  //return weather
+//}
 
-getWeatherAndTimezone().then(data => console.log(data));
+//getWeatherAndTimezone().then(data => console.log(data));
 
-console.log(Temporal.Now.timeZoneId())
+let seconds: number = -7200
+
+// converting seconds to utc format in the hackiest way known to man.
+const date: Date = new Date(null);
+date.setSeconds(seconds);
+let result: string = date.toISOString().slice(11, 16);
+result = "+" + result;
+
+console.log(result)
+let tz = Temporal.TimeZone.from(result)
+let timestamp = Temporal.Instant.fromEpochSeconds(0);
+console.log(tz.getOffsetNanosecondsFor(timestamp))
+
+// The suffering is excellent
